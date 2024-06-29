28a6b05975edb04a81e4b83f92afbd2cb68b5cf8
Carmeshia <65563803+Meshia13@users.noreply.github.com>
2024-06-29 14:21:09 -0400

    style: made changes to css

--- my-card/src/Card.jsx
+++ my-card/src/Card.jsx
@@ -8,25 +8,25 @@ function Card() {
             <h1 className="card-title">
                 Jaguar
             </h1>
-            <img src={animalPic} alt="black jaguar" />
+            <img className='card-image' src={animalPic} alt="black jaguar" />
 
-            <div className='card-facts'>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, dolore libero 
-                    debitis voluptatum eligendi perspiciatis dolorem enim.
+            <div className='card-facts-container'>
+                <p className='card-facts'>
+                Jaguars are the largest of South America’s big cats and the third largest cats in the world.
                 </p>
 
-                <ul>
-                    <li><span>Scientific Name: </span>Jaguar</li>
-                    <li><span>Life Span: </span></li>
-                    <li><span>Characteristics: </span></li>
-                    <li>Another Fact: </li>
+                <ul className='card-list'>
+                    <li><span>Scientific Name: </span>Panthera Onca</li>
+                    <li><span>Life Span: </span>12-15 years</li>
+                    <li><span>Size: </span>3-6.5ft long</li>
+                    <li><span>Weight: </span>100-348lbs</li>
                 </ul>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae tempora perferendis soluta adipisci. 
-                    Quod nihil iure consequatur rerum officiis quisquam, nostrum magnam, pariatur temporibus sed a adipisci 
-                    eius sapiente suscipit vero similique obcaecati delectus veritatis doloribus. Quibusdam sequi alias 
-                    cupiditate nulla numquam ipsam iste deserunt inventore? Aliquam sequi impedit voluptates.
+                <p className='card-description'>
+                    The Jaguar and the Leopard are often confused with one another in zoos. 
+                    Their coloring and markings are so similar that it is difficult for people to distinguish them. 
+                    The difference lies in the center of the Jaguars rosettes, because unlike the leopard, 
+                    the Jaguar has spots inside of its rosettes! The Jag is also a much stockier animal than its cousin, 
+                    with shorter legs and tail, giving it more of a pit bull type appearance.
                 </p>
             </div>
 
